9d546c142ce31d1c1c2455a10fe27a3707ba7562
Gelcy <milevelasco1503@gmail.com>
2025-10-26 19:32:32 -0500

    Subiendo archivos PY y C#

---
+++ Promedio/Promedio/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//LECTURA DE DATOS
+//Paso 1:  Ingresamos las 3 notas 
+Console.WriteLine("Ingrese la primera nota"); 
+int n1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Ingrese la segunda nota"); 
+int n2 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Ingrese la tercera nota"); 
+int n3 = int.Parse(Console.ReadLine());
+
+//PROCESO
+// Paso 2:  Hallamos el promedio de las 3 notas
+int promedio = (n1 + n2 + n3)/3;
+
+//Paso 3: Evaluamos la condicion aprobado (>=11) y desaprobado
+string mje = ""; 
+if (promedio >= 11) 
+    mje = "Aprobado";
+else 
+    mje = "Desaprobado";
+
+//RESULTADOS
+////Mostramos los resultados
+Console.WriteLine("Promedio: " + promedio);
+Console.WriteLine(mje);
